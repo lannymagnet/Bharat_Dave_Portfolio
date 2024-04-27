4fba1701b9015df3388fefd7e715155721886012
lannymagnet <bharatdave00@gmail.com>
2024-04-27 16:35:15 +0530

    Inital Commit Changes

---
+++ src/MainApp.jsx
@@ -0,0 +1,35 @@
+import React, { useState, useEffect, Suspense } from 'react';
+import { Switch, Route } from 'react-router-dom';
+import FallbackSpinner from './components/FallbackSpinner';
+import NavBarWithRouter from './components/NavBar';
+import Home from './components/Home';
+
+function MainApp() {
+  return (
+    <div className="MainApp">
+      <NavBarWithRouter />
+      <main className="main">
+        <Switch>
+          <Suspense fallback={<FallbackSpinner />}>
+            <Route exact path="/" component={Home} />
+            {data
+              && data.sections.map((route) => {
+                const SectionComponent = React.lazy(() => import('./components/' + route.component));
+                return (
+                  <Route
+                    key={route.headerTitle}
+                    path={route.path}
+                    component={() => (
+                      <SectionComponent header={route.headerTitle} />
+                    )}
+                  />
+                );
+              })}
+          </Suspense>
+        </Switch>
+      </main>
+    </div>
+  );
+}
+
+export default MainApp;
